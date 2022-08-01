8809f1dfa03feeeeac2ddc4aa4d0df9e6d68419f
AlexVega2001 <alexander_vega1111@hotmail.com>
2022-08-01 11:22:55 -0500

    Se agrego componente Listados de Autores

---
+++ src/components/ListsAuthors.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export const ListsAuthors = () => {
+  return (
+    <View>
+        <Text>ListsAuthors</Text>
+    </View>
+  )
+}
